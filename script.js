9c51968cf18ea0910d5d5bf4408c6795151e87c5
pacianoseth <feintimg@gmail.com>
2023-07-31 23:05:45 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('div.container');
+const content = document.createElement('div');
+content.classList.add('style');
+content.textContent= 'A';
+
+container.appendChild(content);
+//container.appendChild(content);
+//container.appendChild(content);
