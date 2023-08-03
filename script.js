b96f24ed555041de3f6db77ba8b9a93820f45f9d
pacianoseth <feintimg@gmail.com>
2023-08-03 22:23:20 +0800

    added erase functionality and paint black functionality

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let size = 16;
+let size = 32;
 
 
 let gridCont = document.querySelector("#container");
+let reset = document.querySelector("#erase-all");
+let pix32 = document.querySelector("#pix32");
+let pix64 = document.querySelector("#pix64");
+let pix128 = document.querySelector("#pix128");
+
+gridCont.addEventListener("mousedown", changecolor);
+reset.addEventListener("click", erase);
+//pix32.addEventListener("click", grid);
+//pix64.addEventListener("click", grid);
+//pix128.addEventListener("click", grid);
 
 
 function createGrid(rowSize, colSize) {
@@ -22,6 +32,28 @@ function createGrid(rowSize, colSize) {
 
 }
 
+
+
 grid = createGrid(size, size);
 gridCont.innerHTML = "";
 gridCont.append(...grid);
+
+
+
+function changecolor(e){
+  e.target.setAttribute("style", "background-color:black;");
+}
+
+function erase(){
+const pixels = document.querySelectorAll(".pixel");
+for (let i = 0; i < size * size; i++){
+  pixels[i].setAttribute("style", "background-color:'white';");
+}
+}
+//features to add:
+
+//set a "mode" after clicking a button?
+//a "mode" for erase on pixel at a time?
+//a "mode" for rainbow colors
+//a "mode" for shading colors
+//buttons also for changing the number of pixels on the screen!
